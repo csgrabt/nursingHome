eb65e741d7eafa3f009cb25a2197477f8249981e
Csekő György <gy.cseko@outlook.com>
2021-08-04 11:56:50 +0200

    Address validáció

--- src/main/java/com/example/nursinghome/address/UpdateAddressCommand.java
+++ src/main/java/com/example/nursinghome/address/UpdateAddressCommand.java
@@ -6,17 +6,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Size;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Tag(name = "Set an Address to Elder")
 public class UpdateAddressCommand {
+    @Size(min = 4, max = 4)
     @Schema(defaultValue = "5400")
     private String zipCode;
     @Schema(defaultValue = "Xuzhou")
+    @Size(min = 3, max = 20)
     private String city;
     @Schema(defaultValue = "Kamchatka")
+    @Size(min = 3, max = 20)
     private String street;
+    @Size(min = 1, max = 4)
     @Schema(defaultValue = "54")
     private String houseNumber;
 }
